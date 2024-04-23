91ac72727c70c078f5b390def408e11a3a4f39be
ricrowl <richard392742@gmail.com>
2024-04-23 13:11:46 +0900

    update pdf constructor

--- src/pdf2md.py
+++ src/pdf2md.py
@@ -1,110 +1,165 @@
 import fitz
 from mdutils.mdutils import MdUtils
 import re
-import json
 
 
 class PdfConverter:
-    def __init__(self):
-        pass
+    def __init__(self, pdf_path):
+        with fitz.open(pdf_path) as doc:
+            self.metadata = doc.metadata
+            self.text_block_pages = self.load_text_block(doc)
+            self.toc = doc.get_toc(simple=False)
 
-    def get_title(self, doc):
-        title = doc.metadata["title"]
+    def load_text_block(self, doc):
+        text_blocks = {}
+        for p_num in range(len(doc)):
+            page = doc.load_page(p_num)
+            blocks = page.get_text("blocks")
+            block_details = page.get_text("dict")["blocks"]
+            block_details = [b for b in block_details if "lines" in b.keys()]
+            if len(blocks) != len(block_details):
+                text_coords = [(b["bbox"][0], b["bbox"][1]) for b in block_details]
+            else:
+                text_coords = None
+            block_data_list = []
+            for b_num, b in enumerate(blocks):
+                data = {
+                    "x0": b[0],
+                    "y0": b[1],
+                    "x1": b[2],
+                    "y1": b[3],
+                    "text": b[4],
+                }
+                if text_coords is None:
+                    detail = block_details[b_num]
+                    span = detail["lines"][0]["spans"][0]
+                    data.update(
+                        {
+                            "size": span["size"],
+                            "flags": span["flags"],
+                            "font": span["flags"],
+                            "line_text": span["text"],
+                        }
+                    )
+                else:
+                    target_coord = (data["x0"], data["y0"])
+                    try:
+                        d_num = text_coords.index(target_coord)
+                        detail = block_details[d_num]
+                        span = detail["lines"][0]["spans"][0]
+                        data.update(
+                            {
+                                "size": span["size"],
+                                "flags": span["flags"],
+                                "font": span["flags"],
+                                "line_text": span["text"],
+                            }
+                        )
+                    except ValueError:
+                        data.update(
+                            {
+                                "size": None,
+                                "flags": None,
+                                "font": None,
+                                "line_text": None,
+                            }
+                        )
+                block_data_list.append(data)
+            text_blocks[p_num] = block_data_list
+        return text_blocks
+
+    def get_title(self):
+        title = self.metadata["title"]
         if title is None or title == "":
-            page = doc.load_page(0)
-            text_blocks = page.get_text("blocks")
-            title = text_blocks[0][4]
+            blocks = self.text_block_pages[0]
+            blocks = blocks[: len(blocks) // 2]
+            target_block = max(blocks, key=lambda x: x["size"])
+            title = target_block["text"]
             title = title.replace("\n", "")
         return title
 
-    def get_toc_bookmark(self, doc):
-        toc = doc.get_toc(simple=False)
-        contents = [
-            {
-                "level": level,
-                "title": title,
-                "page": page - 1,
-                "nameddest": info["nameddest"],
-            }
-            for (level, title, page, info) in toc
-        ]
-        return contents
-
-    def get_toc_section(self, doc):
+    def get_toc_bookmark(self):
         contents = []
-        for p_num in range(len(doc)):
-            page = doc.load_page(p_num)
-            text_blocks = page.get_text("dict")["blocks"]
-            for b in text_blocks:
-                if "lines" in b:
-                    for line in b["lines"]:
-                        for span in line["spans"]:
-                            title = span["text"]
-                            if len(title.split(" ")) > 1:
-                                m = re.search(r"^[0-9]+(\.[0-9]+)*", title)
-                                if m:
-                                    level = m.group()
-                                    level = len(level.split("."))
-                                    contents.append(
-                                        {
-                                            "level": level,
-                                            "title": title,
-                                            "page": p_num,
-                                            "size": span["size"],
-                                        }
-                                    )
+        toc = self.toc
+        for level, title, p_num, info in toc:
+            p_num = p_num - 1
+            b_num, b = self.search_block_keyword(p_num, title)
+            if b_num is not None:
+                contents.append(
+                    {
+                        "level": level,
+                        "title": title,
+                        "page": p_num,
+                        "block": b_num,
+                        "size": b["size"],
+                        "title_line_text": b["line_text"],
+                        "nameddest": info["nameddest"],
+                    }
+                )
         return contents
 
-    def add_toc_abstract(self, doc, contents):
+    def get_toc_section(self):
+        contents = []
+        for p_num, blocks in self.text_block_pages.items():
+            for b_num, b in enumerate(blocks):
+                title = b["text"]
+                if len(title.split(" ")) > 1:
+                    m = re.search(r"^[0-9]+(\.[0-9]+)*", title)
+                    if m:
+                        level = m.group()
+                        level = len(level.split("."))
+                        contents.append(
+                            {
+                                "level": level,
+                                "title": title,
+                                "page": p_num,
+                                "block": b_num,
+                                "size": b["size"],
+                                "title_line_text": b["line_text"],
+                            }
+                        )
+        return contents
+
+    def add_toc_abstract(self, contents):
         p_num, title = 0, "Abstract"
-        toc_idx = self.search_toc_block(doc, p_num, title)
-        if toc_idx is not None:
-            extracted_title = toc_idx["block"][4]
-            m = re.search(title, extracted_title, re.IGNORECASE)
-            extracted_title = m.group(0) if m else extracted_title
+        b_num, b = self.search_block_keyword(p_num, title)
+        if b_num is not None:
+            target_title = b["text"]
+            m = re.search(title, target_title, re.IGNORECASE)
+            target_title = m.group(0) if m else target_title
             existed_titles = [c["title"] for c in contents]
-            if extracted_title not in existed_titles:
+            if target_title not in existed_titles:
                 contents.insert(
                     0,
                     {
                         "level": 1,
-                        "title": extracted_title,
+                        "title": target_title,
                         "page": p_num,
+                        "block": b_num,
+                        "size": b["size"],
+                        "title_line_text": b["line_text"],
                     },
                 )
         return contents
 
-    def add_toc_terminal(self, doc, contents):
+    def add_toc_terminal(self, contents):
         title = "References"
-        toc_indice = [
-            self.search_toc_block(doc, c["page"], c["title"]) for c in contents
-        ]
-        # remove no block toc
-        for toc_idx, c in zip(toc_indice, contents):
-            if toc_idx is None:
-                toc_indice.remove(toc_idx)
-                contents.remove(c)
-        toc_fonts = [
-            self.get_block_font(doc, c["page"], idx["index"])
-            for c, idx in zip(contents, toc_indice)
-        ]
-        toc_font_sizes = [font["size"] for font in toc_fonts if font is not None]
-
-        for p_num in range(len(doc)):
-            toc_idx = self.search_toc_block(doc, p_num, title)
-            if toc_idx is not None:
-                extracted_title = toc_idx["block"][4]
-                extracted_font = self.get_block_font(doc, p_num, toc_idx["index"])
-                m = re.search(title, extracted_title, re.IGNORECASE)
-                extracted_title = m.group(0) if m else extracted_title
-                existed_titles = [c["title"].lower() for c in contents]
-                if (
-                    extracted_font["size"] in toc_font_sizes
-                    and extracted_title.lower() not in existed_titles
-                ):
+        toc_sizes = [c["size"] for c in contents]
+        toc_lower_titles = [c["title"].lower() for c in contents]
+        if title.lower() in toc_lower_titles:
+            i = toc_lower_titles.index(title.lower())
+            contents[i].update({"terminal": True})
+            return contents
+        for p_num in self.text_block_pages.keys():
+            b_num, b = self.search_block_keyword(p_num, title)
+            if b_num is not None:
+                if b["size"] in toc_sizes:
+                    target_title = b["text"]
+                    m = re.search(title, target_title, re.IGNORECASE)
+                    target_title = m.group(0) if m else target_title
                     insert_idx = [
                         c["page"] <= p_num
-                        or (c["page"] == p_num and c["block"] < toc_idx["index"])
+                        or (c["page"] == p_num and c["block"] < b_num)
                         for c in contents
                     ]
                     insert_idx = sum(insert_idx)
@@ -112,72 +167,67 @@ class PdfConverter:
                         insert_idx,
                         {
                             "level": 1,
-                            "title": extracted_title,
+                            "title": target_title,
                             "page": p_num,
+                            "block": b_num,
+                            "size": b["size"],
+                            "title_line_text": b["line_text"],
                             "terminal": True,
                         },
                     )
                     break
         return contents
 
-    def get_toc(self, doc):
-        contents = self.get_toc_bookmark(doc)
+    def get_toc(self):
+        contents = self.get_toc_bookmark()
         if len(contents) == 0:
-            contents = self.get_toc_section(doc)
+            contents = self.get_toc_section()
         return contents
 
-    def search_toc_block_match(self, text_blocks, toc_title):
-        blocks = [
-            (i, b, len(b[4])) for i, b in enumerate(text_blocks) if toc_title in b[4]
-        ]
-        return blocks
+    def search_block_keyword(self, page_num, keyword):
 
-    def search_toc_block_lower(self, text_blocks, toc_title):
-        alt_toc_title = toc_title.lower()
-        alt_toc_title = re.sub(r"^[0-9]+(\.[0-9]+)*\s+", "", alt_toc_title)
-        blocks = [
-            (i, b, len(b[4]))
-            for i, b in enumerate(text_blocks)
-            if alt_toc_title in b[4].lower()
-        ]
-        return blocks
-
-    def search_toc_block_number(self, text_blocks, toc_title):
-        m = re.search(r"^[0-9]+(\.[0-9]+)*", toc_title)
-        if m:
-            alt_toc_title = m.group()
+        def search_block_keyword_match(text_blocks, keyword):
             blocks = [
-                (i, b, len(b[4]))
+                (i, b, len(b["text"]))
                 for i, b in enumerate(text_blocks)
-                if alt_toc_title in b[4]
+                if keyword in b["text"]
             ]
-        else:
-            blocks = []
-        return blocks
+            return blocks
 
-    def search_toc_block(self, doc, toc_page, toc_title):
-        page = doc.load_page(toc_page)
-        text_blocks = page.get_text("blocks")
-        start_block_cands = self.search_toc_block_match(text_blocks, toc_title)
+        def search_block_keyword_lower(text_blocks, keyword):
+            alt_toc_title = keyword.lower()
+            alt_toc_title = re.sub(r"^[0-9]+(\.[0-9]+)*\s+", "", alt_toc_title)
+            blocks = [
+                (i, b, len(b["text"]))
+                for i, b in enumerate(text_blocks)
+                if alt_toc_title in b["text"].lower()
+            ]
+            return blocks
+
+        def search_block_keyword_headnumber(text_blocks, keyword):
+            m = re.search(r"^[0-9]+(\.[0-9]+)*", keyword)
+            if m:
+                alt_toc_title = m.group()
+                blocks = [
+                    (i, b, len(b["text"]))
+                    for i, b in enumerate(text_blocks)
+                    if alt_toc_title in b["text"]
+                ]
+            else:
+                blocks = []
+            return blocks
+
+        blocks = self.text_block_pages[page_num]
+        start_block_cands = search_block_keyword_match(blocks, keyword)
         if len(start_block_cands) == 0:
-            start_block_cands = self.search_toc_block_lower(text_blocks, toc_title)
+            start_block_cands = search_block_keyword_lower(blocks, keyword)
         if len(start_block_cands) == 0:
-            start_block_cands = self.search_toc_block_number(text_blocks, toc_title)
+            start_block_cands = search_block_keyword_headnumber(blocks, keyword)
         if len(start_block_cands) == 0:
-            return None
+            return None, None
         start_block = min(start_block_cands, key=lambda x: x[2])
-        block_index, block = start_block[0], start_block[1]
-        return {"index": block_index, "block": block}
-
-    def get_block_font(self, doc, page_num, block_index):
-        page = doc.load_page(page_num)
-        text_blocks = page.get_text("dict")["blocks"]
-        block = text_blocks[block_index]
-        if "lines" in block:
-            span = block["lines"][0]["spans"][0]
-            return {"font": span["font"], "size": float(span["size"])}
-        else:
-            return None
+        b_num, block = start_block[0], start_block[1]
+        return b_num, block
 
     def filter_texts(self, texts):
         rejected_regexs = [
@@ -198,49 +248,47 @@ class PdfConverter:
             texts = [t for t in texts if not bool(reg.match(t))]
         return texts
 
-    def read_pdf_document(self, pdf_path):
-        with fitz.open(pdf_path) as doc:
-            # get title
-            title = self.get_title(doc)
-            # get TOC
-            contents = self.get_toc(doc)
-            # add no number toc
-            contents = self.add_toc_abstract(doc, contents)
-            contents = self.add_toc_terminal(doc, contents)
-            # get block index
-            toc_indice = [
-                self.search_toc_block(doc, c["page"], c["title"]) for c in contents
-            ]
-            # remove no block toc
-            for toc_idx, c in zip(toc_indice, contents):
-                if toc_idx is None:
-                    toc_indice.remove(toc_idx)
-                    contents.remove(c)
-            # extract text
-            for i, (toc_idx, c) in enumerate(zip(toc_indice, contents)):
-                if i < len(contents) - 1:
-                    end_page = contents[i + 1]["page"]
-                    end_idx = toc_indice[i + 1]["index"]
-                else:
-                    end_page = len(doc) - 1
-                    end_idx = None
-                texts = []
-                for p_num in range(c["page"], end_page + 1):
-                    page = doc.load_page(p_num)
-                    text_blocks = page.get_text("blocks")
-                    if p_num == end_page and end_idx is not None:
-                        text_blocks = text_blocks[:end_idx]
-                    if p_num == c["page"]:
-                        text_blocks = text_blocks[toc_idx["index"] :]
-                    extracted = [b[4] for b in text_blocks]
-                    extracted = self.filter_texts(extracted)
-                    texts += extracted
-                contents[i].update({"texts": texts})
-            self.add_toc_abstract(doc, contents)
+    def construct_document(self):
+        # get title
+        title = self.get_title()
+        # get TOC
+        contents = self.get_toc()
+        # add no numbering TOC
+        contents = self.add_toc_abstract(contents)
+        contents = self.add_toc_terminal(contents)
+        # limit TOC range to terminal
+        limited_contents = []
+        limited = False
+        for c in contents:
+            limited_contents.append(c)
+            if "terminal" in c.keys() and c["terminal"]:
+                limited = True
+                break
+        if not limited:
+            limited_contents.append(
+                {
+                    "page": max(self.text_block_pages.keys()),
+                    "block": None,
+                }
+            )
+        contents = limited_contents
 
-            print(f"{title}\n===")
-            for c in contents:
-                print(f"{c}\n")
+        # extract text
+        for i, c in enumerate(contents[:-1]):
+            end_page_num = contents[i + 1]["page"]
+            end_block_num = contents[i + 1]["block"]
+            texts = []
+            for p_num in range(c["page"], end_page_num + 1):
+                text_blocks = self.text_block_pages[p_num]
+                if p_num == end_page_num and end_block_num is not None:
+                    text_blocks = text_blocks[:end_block_num]
+                if p_num == c["page"]:
+                    text_blocks = text_blocks[c["block"] :]
+                extracted = [b["text"] for b in text_blocks]
+                extracted = self.filter_texts(extracted)
+                texts += extracted
+            contents[i].update({"texts": texts})
+        contents = contents[:-1]
 
         return {"title": title, "contents": contents}
 
@@ -255,15 +303,13 @@ class PdfConverter:
                 mdFile.new_line(t)
         mdFile.create_md_file()
 
-    def run(self, pdf_path, md_path):
-        document = self.read_pdf_document(pdf_path)
+    def run(self, md_path):
+        document = self.construct_document()
         self.write_md(document, md_path)
 
 
 def main():
-    PdfConverter().run(
-        "./data/pdf/sample.pdf", "./data/pdf/sample.pdf.md"
-    )
+    PdfConverter("./data/pdf/sample.pdf").run("./data/pdf/sample.pdf.md")
 
 
 if __name__ == "__main__":
